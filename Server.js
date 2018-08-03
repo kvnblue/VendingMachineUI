ccd3eaa0ce68e4f5a940a407713099f65fa58094
kvnblue <kvnblue@gmail.com>
2018-08-03 06:01:02 +0200

    Updated Path in server.js file and added engines block in package.json

--- Server.js
+++ Server.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+const path = require('path');
+app.use(express.static(__dirname + '/dist/vending-app'));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/vending-app/index.html'));
+  });
+var server_port = process.env.PORT || 4200;
+var server_host = '0.0.0.0';
+
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
